a24cdf22762c033471e828ae275182c79ae9d847
elveli <elveli@gmail.com>
2026-03-21 21:37:02 -0700

    feat(kargo): Configure Kargo admin credentials
    Set sensitive values for Kargo's admin account password hash and token signing key in the Terraform configuration. This enables secure access to the Kargo API.

--- terraform/kargo.tf
+++ terraform/kargo.tf
@@ -24,5 +24,15 @@ resource "helm_release" "kargo" {
   chart      = "kargo"
   version    = "0.8.0"
 
+  set_sensitive {
+    name  = "api.adminAccount.passwordHash"
+    value = "$2b$10$LJ88mIK6ApAHwaiWSJVmt.1a4HDc95iTp/p/UvWQLfbWvQ1/kKFb."
+  }
+
+  set_sensitive {
+    name  = "api.adminAccount.tokenSigningKey"
+    value = "8e6bda8fd47f6b605325d689f0777a70989edfd3d77a19983e897b60e3d78a8d"
+  }
+
   depends_on = [helm_release.argocd]
 }
